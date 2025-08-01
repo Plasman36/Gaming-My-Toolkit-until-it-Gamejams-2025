61c0d7febc0c9813c5d14370aed102980fbdeba8
Plasman36 <joshuaCai2007@gmail.com>
2025-07-31 17:59:45 -0700

    Start of Main Menu and Settings, + Level Selector

---
+++ You, Again/Assets/Scripts/Canvas Scripts/Options Menu.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void setMasterVolume(float volume)
+    {
+        mixer.SetFloat("MasterVolume", volume);
+    }
+}
