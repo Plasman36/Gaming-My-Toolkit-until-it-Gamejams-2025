83331536dffe11f692f510dfaf4212985e65babe
Rhygon1 <98714172+Rhygon1@users.noreply.github.com>
2025-08-01 16:01:34 -0400

    Revert "Merge branch 'main' of https://github.com/Plasman36/Gaming-My-Toolkit-until-it-Gamejams-2025"
    This reverts commit d28f31223590bb83201eb8425563f6434e8ae1e5, reversing
    changes made to 233cfa08f9748b33026ff318b5c8a8e60b311c3b.

--- You, Again/Assets/Scripts/MovingPlatform.cs
+++ You, Again/Assets/Scripts/MovingPlatform.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
 
 public class MovingPlatform : MonoBehaviour
 {
@@ -6,15 +8,93 @@ public class MovingPlatform : MonoBehaviour
     public Transform start;
     public Transform stop;
 
+    [Header("Object Handling")]
+    public int raycastCount = 10;
+    public float platformWidth;
+    public float maxGapDistance = 0.2f;
+    public HashSet<GameObject> ObjectsOnPlatform = new HashSet<GameObject>();
+
     Transform platform;
     Rigidbody2D platformRB;
     private bool right = true;
+
+    [Header("Debug Settings")]
+    public bool showDebugRays = true;
+    public float debugRayLength = 10f;
+    public Color debugRayColorEmpty = Color.green;
+    public Color debugRayColorHit = Color.red;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         platform = GetComponent<Transform>();
         platformRB = GetComponent<Rigidbody2D>();
+        platformWidth = transform.localScale.x;
+    }
+
+    HashSet<GameObject> GetObjectsOnPlatform()
+    {
+        HashSet<GameObject> processedObjects = new HashSet<GameObject>();
+
+        for (int i = 0; i < raycastCount; i++)
+        {
+            float rayOffset = (i / (float)(raycastCount - 1) - 0.5f) * platformWidth;
+            float rayStartOffset = 0.05f; // Just above the platform
+            Vector3 rayStart = transform.position + new Vector3(rayOffset, rayStartOffset, 0);
+
+
+            RaycastHit2D[] hits = Physics2D.RaycastAll(rayStart, Vector2.up, Mathf.Infinity, ~LayerMask.GetMask("Ground"));
+
+            var sortedHits = hits.OrderBy(hit => hit.distance).ToArray();
+
+            ProcessRaycastHits(sortedHits, processedObjects, out float maxRelevantDistance);
+
+
+            if (showDebugRays)
+            {
+                // Use the actual distance where objects contribute, or default length if no objects
+                float debugLength = maxRelevantDistance > 0 ? maxRelevantDistance : debugRayLength;
+                Color rayColor = processedObjects.Count > 0 ? debugRayColorHit : debugRayColorEmpty;
+                Debug.DrawRay(rayStart, Vector2.up * debugLength, rayColor);
+            }
+        }
+
+
+
+        return processedObjects;
+    }
+
+    void ProcessRaycastHits(RaycastHit2D[] sortedHits, HashSet<GameObject> processedObjects, out float maxRelevantDistance)
+    {
+        float lastDistance = 0f;
+        maxRelevantDistance = 0f;
+
+        foreach (RaycastHit2D hit in sortedHits)
+        {
+            if (processedObjects.Contains(hit.collider.gameObject))
+                continue;
+
+            float gap = hit.distance - lastDistance;
+            if (gap > maxGapDistance)
+            {
+                break;
+            }
+
+            processedObjects.Add(hit.collider.gameObject);
+
+            BoxCollider2D boxCollider = hit.collider.GetComponent<BoxCollider2D>();
+            if (boxCollider != null)
+            {
+                lastDistance = hit.distance + boxCollider.size.y;
+                maxRelevantDistance = lastDistance;
+            }
+            else
+            {
+                lastDistance = hit.distance;
+                maxRelevantDistance = lastDistance;
+            }
+
+        }
     }
 
     // Update is called once per frame
@@ -32,7 +112,8 @@ public class MovingPlatform : MonoBehaviour
             {
                 right = false;
             }
-        } else
+        }
+        else
         {
             platformRB.linearVelocity = speed * Time.deltaTime * (start.position - platform.position).normalized;
             if (platform.position == start.position)
@@ -41,5 +122,11 @@ public class MovingPlatform : MonoBehaviour
             }
         }
 
+        HashSet<GameObject> currentObjects = GetObjectsOnPlatform();
+        foreach (GameObject curr in currentObjects)
+        {
+            Rigidbody2D RB = curr.GetComponent<Rigidbody2D>();
+            RB.linearVelocity = platformRB.linearVelocity;
+        }
     }
 }
