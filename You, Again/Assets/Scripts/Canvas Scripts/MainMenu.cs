58dea5259d00b2316cb7c4dd3b6226e1a2aff776
diops5629 <56743406+diops5629@users.noreply.github.com>
2025-08-02 11:10:37 -0700

    Added transitions to the main menu

--- You, Again/Assets/Scripts/Canvas Scripts/MainMenu.cs
+++ You, Again/Assets/Scripts/Canvas Scripts/MainMenu.cs
@@ -1,15 +1,30 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
+    public Animator transition;
+    public float timeBetween;
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        loadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+    void loadScene(int scene)
+    {
+        Debug.Log("Started coroutine");
+        StartCoroutine(transit(scene));
+    }
+    IEnumerator transit(int scene)
+    {
+        transition.SetTrigger("Start");
+        Debug.Log("Triggering Animation");
+        yield return new WaitForSeconds(timeBetween);
+        SceneManager.LoadScene(scene);
     }
 }
